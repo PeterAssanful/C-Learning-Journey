278f16b652f60f50fa2e00b77a45d9e48a04b69c
Peter <realpeterdev@gmail.>
2025-09-08 11:05:44 +0000

    Feat: Add if-else statement for siblings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,3 +15,14 @@ int numberOfSiblings= 9;
 
 Console.WriteLine($"My favouritt food is {favoritFood}");
 Console.WriteLine($"I have {numberOfSiblings} siblings.");
+
+if (userAge > 18)
+    { 
+    
+    Console.WriteLine("Access granted: You're an adult");
+    
+    }
+else
+{
+    Console.WriteLine("Access Denied: You still mama's breast milk");
+}
